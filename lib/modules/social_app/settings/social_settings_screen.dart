27a94704685ec18c265f4a9597e88f04f76c50b7
abdullah_mansour <abdullah.mansour97@gmail.com>
2021-06-12 21:17:59 +0200

    facebook login

--- lib/modules/social_app/settings/social_settings_screen.dart
+++ lib/modules/social_app/settings/social_settings_screen.dart
@@ -1,11 +1,14 @@
 import 'package:conditional_builder/conditional_builder.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:softagi_2021/layout/social_app/cubit/cubit.dart';
 import 'package:softagi_2021/layout/social_app/cubit/states.dart';
 import 'package:softagi_2021/modules/social_app/login/social_sign_in_screen.dart';
 import 'package:softagi_2021/shared/components/components.dart';
+import 'package:softagi_2021/shared/network/remote/dio_helper.dart';
 
 class SocialSettingsScreen extends StatelessWidget {
   @override
@@ -24,14 +27,15 @@ class SocialSettingsScreen extends StatelessWidget {
                   width: 100.0,
                   height: 100.0,
                   child: Image(
-                    image: SocialCubit.get(context).profileImage == null ? NetworkImage(
-                        SocialCubit.get(context).socialUserModel.image) : FileImage(SocialCubit.get(context).profileImage),
+                    image: SocialCubit.get(context).profileImage == null
+                        ? NetworkImage(
+                            SocialCubit.get(context).socialUserModel.image)
+                        : FileImage(SocialCubit.get(context).profileImage),
                     fit: BoxFit.cover,
                   ),
                 ),
                 OutlinedButton(
-                  onPressed: ()
-                  {
+                  onPressed: () {
                     SocialCubit.get(context).getProfileImage();
                   },
                   child: Text(
@@ -51,11 +55,11 @@ class SocialSettingsScreen extends StatelessWidget {
                   SocialCubit.get(context).socialUserModel.bio,
                 ),
                 OutlinedButton(
-                  onPressed: ()
-                  {
-                    FirebaseAuth.instance.signOut().then((value) {
-                      navigateAndFinish(context: context, widget: SocialSignInScreen());
-                    });
+                  onPressed: () {
+                    // FirebaseAuth.instance.signOut().then((value) {
+                    //   navigateAndFinish(context: context, widget: SocialSignInScreen());
+                    // });
+                    facebookLogin();
                   },
                   child: Text(
                     'Logout',
@@ -70,5 +74,49 @@ class SocialSettingsScreen extends StatelessWidget {
         );
       },
     );
+  }
+
+  void facebookLogin() async {
+    final LoginResult result = await FacebookAuth.instance.login(
+      permissions: [],
+    );
+
+    // by default we request the email and the public profile
+
+    // or FacebookAuth.i.login()
+
+    if (result.status == LoginStatus.success) {
+      // you are logged
+      print(result.accessToken.token);
+      print(result.accessToken.userId);
+      print(result.message);
+
+      Dio dio = Dio(
+        BaseOptions(
+          baseUrl: 'https://graph.facebook.com/',
+          receiveDataWhenStatusError: true,
+        ),
+      );
+
+      dio.get('v2.12/me', queryParameters:
+      {
+        'fields': 'name,first_name,last_name,picture',
+        'access_token': result.accessToken.token,
+      }).then((value) {
+        print(value.data);
+
+        dio.get('${result.accessToken.userId}/picture', queryParameters: {
+          'height' : 1024,
+          'width' : 1024,
+        },).then((value)
+        {
+          print(value.data);
+        });
+
+      }).catchError((error)
+      {
+        print(error.toString());
+      });
+    }
   }
 }
--- lib/shared/network/local/cache_helper.dart
+++ lib/shared/network/local/cache_helper.dart
@@ -1,13 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 class CacheHelper {
-  static SharedPreferences sharedPreferences;
+  //static SharedPreferences sharedPreferences;
 
   static init() async
   {
-    sharedPreferences = await SharedPreferences.getInstance();
+    //sharedPreferences = await SharedPreferences.getInstance();
   }
 
   static Future<bool> setData({
@@ -15,17 +15,17 @@ class CacheHelper {
     @required value,
   })
   {
-    if (value is String)
-      return sharedPreferences.setString(key, value);
-    else if (value is bool)
-      return sharedPreferences.setBool(key, value);
-    else if (value is int) return sharedPreferences.setInt(key, value);
-    return sharedPreferences.setDouble(key, value);
+    // if (value is String)
+    //   return sharedPreferences.setString(key, value);
+    // else if (value is bool)
+    //   return sharedPreferences.setBool(key, value);
+    // else if (value is int) return sharedPreferences.setInt(key, value);
+    // return sharedPreferences.setDouble(key, value);
   }
 
   static dynamic getData({
     @required String key,
   }) {
-    return sharedPreferences.get(key);
+    // return sharedPreferences.get(key);
   }
 }
